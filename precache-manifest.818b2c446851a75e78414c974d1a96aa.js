240144918f35753cd545efdcc28a3512a788f6a3
Joe <christowel@live.fr>
2020-04-24 21:56:34 -0400

    Updates

---
+++ precache-manifest.818b2c446851a75e78414c974d1a96aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ae35fa6c3f400a95974a597557cc35",
+    "url": "/robots-react/index.html"
+  },
+  {
+    "revision": "c1075cf6d089a64d1a51",
+    "url": "/robots-react/static/css/main.80b78eb7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/robots-react/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-react/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1075cf6d089a64d1a51",
+    "url": "/robots-react/static/js/main.04b549e6.chunk.js"
+  },
+  {
+    "revision": "84a78fbae3716775d560",
+    "url": "/robots-react/static/js/runtime-main.5060ef7e.js"
+  }
+]);
